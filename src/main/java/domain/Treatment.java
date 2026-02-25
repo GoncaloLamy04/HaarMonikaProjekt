d9d1b387a5a7b668bbe7b0ded78623ed389bcb21
GoncaloLamy04 <god001@edu.zealand.dk>
2026-02-25 12:27:28 +0100

    Projekt Hårmonika

--- src/main/java/domain/Treatment.java
+++ src/main/java/domain/Treatment.java
@@ -1,23 +1,17 @@
 package domain;
 
 public class Treatment {
-    private int id;
     private String treatmentType;
 
-    public Treatment(int time, String treatmentType){
-        this.id = id;
+    public Treatment(String treatmentType){
         this.treatmentType = treatmentType;
     }
-
-    public int getTime(){return id;}
-    public void setTime(){this.id = id;}
 
     public String getTreatmentType(){return treatmentType;}
     public void setTreatmentType(){this.treatmentType = treatmentType;}
 
-
     @Override
     public String toString(){
-        return "Treatment{" + treatmentType + "," + id + "}";
+        return "Treatment{"+ treatmentType +"}";
     }
 }
